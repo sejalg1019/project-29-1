d407a9eaf50ab939b3c585e22cbc88a5d1c8c0a5
sejalg1019 <73032519+sejalg1019@users.noreply.github.com>
2020-12-05 13:15:43 -0500

    added-files

---
+++ Box.js
@@ -0,0 +1,21 @@
+class Box{ 
+  constructor(x,y,width,height){ 
+      var options = { 
+          restitution: 0.3
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world,this.body);
+  }
+  display(){
+      var pos = this.body.position;
+      var angle = this.body.angle; 
+      push(); 
+      translate(pos.x, pos.y); 
+      rectMode(CENTER);
+      fill(135,206,234);
+      rect(0,0,this.width,this.height);
+      pop(); 
+  }
+}
